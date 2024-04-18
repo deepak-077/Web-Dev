10f8bb3d0cf8be866ba577a6af61ffd36bad1e83
Deepak Kumar <125119545+deepak-077@users.noreply.github.com>
2024-04-18 06:57:23 +0000

    updated date

--- 01_basics/date.js
+++ 01_basics/date.js
@@ -1,2 +1,14 @@
 let mydate=new Date();
-console.log(mydate.toDateString());
+console.log(mydate.toDateString());//readable form of date
+console.log(mydate.toString()); // includes gmt time and coodinates
+
+console.log(mydate.toLocaleDateString()); //date in daya to day use form
+
+
+//own created format of date
+
+let myCreatedDate = new Date(2023, 0, 23)
+
+// let myCreatedDate = new Date("01-14-2023")
+// let myCreatedDate = new Date("2023-01-14") // both format prints same result
+console.log(myCreatedDate.toDateString());
