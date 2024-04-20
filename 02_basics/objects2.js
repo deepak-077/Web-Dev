27bb6f1a9f69b08d6a51bf9f5fd0aa9335b3c1c0
Deepak Kumar <125119545+deepak-077@users.noreply.github.com>
2024-04-20 09:50:01 +0000

    objects completed

--- 02_basics/objects2.js
+++ 02_basics/objects2.js
@@ -5,7 +5,7 @@ car.model="urus performante";
 
 car.price="5cr";
 
-console.log(car);
+// console.log(car);
 
 //concatenating objects
 
@@ -24,7 +24,9 @@ const obj3 = {...obj1, ...obj2} //spread operator
 // console.log(obj3);
 
 
-console.log(Object.keys(car));//similarly we can print values
+// console.log(Object.keys(car)); //it returns all keys in the form of an array we can use it later on using loops 
+
+//similarly we can print values
 
 //data comes in the form of array when it comes to database 
 const users = [
@@ -40,4 +42,50 @@ const users = [
         id: 1,
         email: "h@gmail.com"
     },
+]
+
+// then we can access them using loops or normal array variables
+// ex 
+console.log(users[1]);
+
+//we can now use .operator to access its variables
+console.log(users[1].email);
+
+console.log(Object.entries(car)); //return key, value pair in the form of an array
+
+console.log(users.hasOwnProperty('isLoggedIn')); //return boolean value
+
+//its important if we are performing some operation on database and if there is no such value it will crash. So we can check before hand to avoid crash
+
+
+
+// ----------------------   DE-STRUCTUTRING OBJECTS ----------------------------
+// value to be extracted     source 
+const {company}=          car;//we can change the key
+const {company:com}=      car;
+
+
+console.log(company);
+console.log(com);
+
+
+
+
+// ------------------------------------- API ------------------------------
+
+//we get api from back end in 2 forms 
+ //1 its like objects but it doesn't have name and keys are also strings
+
+//  {
+//     "name": "chai or code",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+
+//2 in the form of an array
+[
+    {},
+    {},
+    {}
 ]
