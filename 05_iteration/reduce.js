31c352a2c9bcd272ba841ba3d4e38ac9e6fef3ac
Deepak Kumar <125119545+deepak-077@users.noreply.github.com>
2024-04-23 13:38:40 +0000

    reduce function in js

---
+++ 05_iteration/reduce.js
@@ -0,0 +1,8 @@
+// reduce is used basically in e-com sites where we have to add every item in cart
+ 
+// its function is similar to declaring a sum variable outside for loop and iterating over the array to find the sum.
+
+// ex- int sum=0;
+// for(int i=0i<arr.length;i++){
+//     sum=arr[i]+sum;
+// }
